57d0a1b12686f67760c51a32723d73e4bf61028e
songrun <songrunmo@gmail.com>
2025-01-19 11:16:04 +0800

    add: comic dark style

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+use slint_build::CompilerConfiguration;
+
 fn main() {
-    slint_build::compile("ui/app.slint").expect("build slint failed");
+    let config = CompilerConfiguration::new().with_style("cosmic-dark".to_string());
+    slint_build::compile_with_config("ui/app.slint", config).expect("build slint failed");
 }
