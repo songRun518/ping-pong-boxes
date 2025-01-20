67f179abdddc08298e571bf67c381dd5ba69cc66
songrun <songrunmo@gmail.com>
2025-01-20 12:54:05 +0800

    cahgne: style

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use slint_build::CompilerConfiguration;
 
 fn main() {
-    let config = CompilerConfiguration::new().with_style("cosmic-dark".to_string());
+    let config = CompilerConfiguration::new().with_style("fluent-dark".to_string());
     slint_build::compile_with_config("ui/app.slint", config).expect("build slint failed");
 }
